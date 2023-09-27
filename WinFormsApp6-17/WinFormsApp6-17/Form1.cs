d1b0cb2da53fd5e95a2b4c8fec4430042d7d6d48
Jonathan Scholl <78108494+jpscholl@users.noreply.github.com>
2023-09-27 18:51:46 -0400

    Add files via upload

---
+++ WinFormsApp6-17/WinFormsApp6-17/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp6_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string messageTitle = "Greetings!";
+            string messageText = "Hello there good sir/madam!";
+
+
+            MessageBox.Show(messageText, messageTitle);
+            MessageBox.Show("This is another way to show this box", "Howdy");
+
+
+            string message2 = "Do you want to close window?";
+            string title2 = "Close window";
+
+            MessageBoxButtons button2 = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message2, title2, button2);
+            if(result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else if(result == DialogResult.No)
+            {
+                //nothing
+            }
+
+            string message3 = "Did you see icon?";
+            string title3 = "ok";
+            MessageBox.Show(message3, title3, MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+        }
+    }
+}
